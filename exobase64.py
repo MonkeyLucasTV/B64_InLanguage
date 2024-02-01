19013c3f6b8d045821f72b1d74cf1d877332c601
MonkeyLucas <69850001+MonkeyLucasTV@users.noreply.github.com>
2024-02-01 14:46:44 +0100

    Amelioré

--- exobase64.py
+++ exobase64.py
@@ -15,5 +15,34 @@ def InitialiserTable():
     print(tableB64)
     
     
+def AfficherBinaire(entier, nbBits):
+    for i in range(i, nbBits): 
+        print("%d",(entier >> (nbBits-i-1))&0x01)
         
+def AfficherTable():
+    for i in range(64):
+        print("%.2d : ",i)
+        AfficherBinaire(i,6)
+        print("\t%c\t", tableB64[i])
+        if((i+1)%4==0): print("\n")
+
+def Code3octetBase64(code,donnee):
+    nb24bits = (donnee[0]<<16) + (donnee[1]<<8) + donnee[2]
+    AfficherBinaire(nb24bits)
+    print("\n") 
+    code[4] = 0
+    code[3] = tableB64[nb24bits&0x3f]
+    nb24bits >>= 6
+    code[2] = tableB64[nb24bits&0x3f]
+    nb24bits >>= 6
+    code[1] = tableB64[nb24bits&0x3f]
+    nb24bits >>= 6
+    code[1] = tableB64[nb24bits&0x3f]
+
+def codeNoctetsB64(code,donnee,nbOctetsDonnees):
+    ResteEnf12Seq = (nbOctetsDonnees*8)%24
+    print("reste en fin de sequence : %d bits\n", ResteEnf12Seq)
+    if ResteEnf12Seq==16:
+        donnee[nbOctetsDonnees+1] = 0
+          
         
